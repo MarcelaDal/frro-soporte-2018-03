2e6de0dcd1dbcceda03d65eccb5c07636b7557d1
Marcela Dalpra <marcelaldalpra@gmail.com>
2018-04-20 12:35:55 -0300

    ejercicio 2

--- practico03/ejercicio2.py
+++ practico03/ejercicio2.py
@@ -1,11 +1,29 @@
 import pymysql
+import datetime
 
-conector = pymysql.connect(host='localhost', db='tp3', user='root', password='')
-cursor = conector.cursor()
 
-sql = 'INSERT INTO Persona(nombre ,fecha_nacimiento, dni, altura) VALUES ("tomas","31101996", "39951593", 182)'
-cursor.execute(sql)
-cursor.execute('SELECT * FROM Persona')
-select = cursor.fetchall()
-print(select)
-conector.commit()
+conn= pymysql.connect(host="localhost", port=3306, db="tp3", user="root", password="")
+
+cur= conn.cursor()
+
+print('Antes de ejecutar la consulta:')
+cur.execute("SELECT * FROM personas")
+result= cur.fetchall()
+columns = [column[0] for column in cur.description]
+for row in result:
+    print(dict(zip(columns, row)))
+
+date = datetime.datetime.today()
+d= date.strftime('%Y-%m-%d %H:%M:%S')
+query = "INSERT into personas values (%s,%s,%s,%s,%s)"
+
+cur.execute(query, [0 ,"Juan", d , 15437424, 1])
+conn.commit()
+
+print('Luego de ejecutar la consulta:')
+cur.execute("SELECT * FROM personas")
+result= cur.fetchall()
+columns = [column[0] for column in cur.description]
+for row in result:
+    print(dict(zip(columns, row)))
+
